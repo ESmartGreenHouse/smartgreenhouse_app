f459ab077babdf22e820b6299a517859520eec14
Jannik Rhode <jannik.rhode@hs-osnabrueck.de>
2020-11-06 11:49:07 +0100

    Improve login ui for mobile and web

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -2,6 +2,7 @@ import 'package:authentication_repository/authentication_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smartgreenhouse_app/login/login.dart';
+import 'package:smartgreenhouse_app/theme.dart';
 
 class LoginPage extends StatelessWidget {
   static Route route() {
@@ -11,16 +12,21 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
+      backgroundColor: GreenHouseColors.green,      
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/greenhouse.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: BlocProvider(
           create: (_) => LoginCubit(
             context.repository<AuthenticationRepository>(),
           ),
           child: LoginForm(),
         ),
-      ),
+      ),            
     );
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 
-final theme = ThemeData(
-  primaryColorDark: const Color(0xFF0097A7),
-  primaryColorLight: const Color(0xFFB2EBF2),
-  primaryColor: const Color(0xFF00BCD4),
-  accentColor: const Color(0xFF009688),
-  scaffoldBackgroundColor: const Color(0xFFE0F2F1),
-  inputDecorationTheme: InputDecorationTheme(
-    border: OutlineInputBorder(
-      borderRadius: BorderRadius.circular(8),
+class GreenHouseColors {
+  static const green = Color(0xff095F00);
+  static const orange = Color(0xffFF792D);
+  static const black = Color(0xff575756);
+  static const blue = Color(0xff0069b4);
+}
+
+class GreenHouseTheme {
+  static final light = ThemeData.light().copyWith(
+    backgroundColor: Colors.white,
+    primaryColor: GreenHouseColors.green,   // depricated
+    accentColor: GreenHouseColors.orange,   // depricated
+    disabledColor: GreenHouseColors.black,
+    colorScheme: ColorScheme.light().copyWith(
+      primary: GreenHouseColors.green,
+      secondary: GreenHouseColors.orange,
     ),
-  ),
-);
+    floatingActionButtonTheme: FloatingActionButtonThemeData(
+      backgroundColor: GreenHouseColors.orange,
+      foregroundColor: Colors.white,      
+    ),
+    cardTheme: CardTheme(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.0)),
+    ),
+    dialogTheme: DialogTheme(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24.0)),
+    )
+  );
+}
