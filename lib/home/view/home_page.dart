8ed2633300751c439f61136f5acbba0cef643712
Jannik Rhode <jannik.rhode@hs-osnabrueck.de>
2020-11-23 12:54:09 +0100

    Add responsive scaffold

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:responsive_scaffold/templates/layout/scaffold.dart';
 import 'package:smartgreenhouse_app/authentication/authentication.dart';
-import 'package:smartgreenhouse_app/home/home.dart';
 
 class HomePage extends StatelessWidget {
   static Route route() {
@@ -12,18 +13,69 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final user = context.bloc<AuthenticationBloc>().state.user;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: <Widget>[
+
+    return ResponsiveScaffold(
+      title: Text('Home'),
+      drawer: ListView(
+        children: [
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.microchip),
+            title: Text('Devices'),
+          ),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.microchip),
+            title: Text('Devices'),
+          ),
+          ListTile(
+            leading: FaIcon(FontAwesomeIcons.microchip),
+            title: Text('Devices'),
+          ),
           IconButton(
             key: const Key('homePage_logout_iconButton'),
             icon: const Icon(Icons.exit_to_app),
             onPressed: () => context
                 .bloc<AuthenticationBloc>()
                 .add(AuthenticationLogoutRequested()),
-          )
+          ),
         ],
+      ),
+      endIcon: Icons.help,
+      endDrawer: ListView(
+        children: [
+          ListTile(
+            leading: Icon(Icons.filter_list),
+            title: Text('Filter List'),
+            subtitle: Text('Hide and show items'),
+            trailing: Switch(
+              value: true,
+              onChanged: (val) {},
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.image_aspect_ratio),
+            title: Text('Size Settings'),
+            subtitle: Text('Change size of images'),
+          ),
+          ListTile(
+            title: Slider(
+              value: 0.5,
+              onChanged: (val) {},
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.sort_by_alpha),
+            title: Text('Sort List'),
+            subtitle: Text('Change layout behavior'),
+            trailing: Switch(
+              value: false,
+              onChanged: (val) {},
+            ),
+          ),
+        ],
+      ),
+      trailing: IconButton(
+        icon: Icon(Icons.search),
+        onPressed: () {},
       ),
       body: Align(
         alignment: const Alignment(0, -1 / 3),
@@ -35,6 +87,11 @@ class HomePage extends StatelessWidget {
             Text(user.name ?? '', style: textTheme.headline5),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Theme.of(context).accentColor,
+        onPressed: () {},
       ),
     );
   }
